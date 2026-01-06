bc2869f4fde6c511107f1e4b5596eaa43d9a5ca8
EmilioPastor <emiliopastorzurita906@gmail.com>
2026-01-06 22:43:37 +0100

    Migraria-ERP-V.2.2.8

--- src/app/admin/calendar/page.tsx
+++ src/app/admin/calendar/page.tsx
@@ -1,4 +1,5 @@
-  export const dynamic = "force-dynamic";
+ export const revalidate = 0;
+ export const dynamic = "force-dynamic";
 
   import { supabaseAdmin } from "@/lib/supabaseAdmin";
   import Link from "next/link";
