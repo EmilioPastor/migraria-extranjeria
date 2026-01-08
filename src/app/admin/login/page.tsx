567e4d7e475d4f900dad25fa983d35331e823db9
EmilioPastor <emiliopastorzurita906@gmail.com>
2026-01-08 20:22:51 +0100

    Migraria-ERP-V.2.3.1

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabaseBrowser } from "@/lib/supabaseBrowser";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function AdminLoginPage() {
   const router = useRouter();
